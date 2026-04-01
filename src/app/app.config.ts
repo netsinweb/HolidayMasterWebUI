e2e947286567b7f73372ec71d3830526ffc802e4
netsinweb <nets@mac.com>
2026-04-01 20:55:34 +0900

    Add: HttpClientを追加

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 
 import { providePrimeNG } from 'primeng/config';
@@ -8,6 +9,7 @@ import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     providePrimeNG({
